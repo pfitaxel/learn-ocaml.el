b3b0b939da9a8dffe5d1b6811043b4c9c3d44f58
Erik Martin-Dorel <erik@martin-dorel.org>
2020-01-12 02:45:22 +0100

    test: Attempt to extract/inject learn-ocaml-client

--- test.sh
+++ test.sh
@@ -16,23 +16,53 @@ red () {
 
 green "Beforehand: LEARNOCAML_IMAGE=$LEARNOCAML_IMAGE"
 # Default learn-ocaml image
-: ${LEARNOCAML_IMAGE:=leunam217/learn-ocaml}
+: ${LEARNOCAML_IMAGE:=ocamlsf/learn-ocaml}
 # Do "export LEARNOCAML_IMAGE=…" before running test.sh to override
 green "Henceforth: LEARNOCAML_IMAGE=$LEARNOCAML_IMAGE\n"
 
-# green "Beforehand: CLIENT_VERSION=$CLIENT_VERSION"
-# # Default learn-ocaml version
-# : ${CLIENT_VERSION:=ocamlsf/learn-ocaml-client}
-# # Do "export CLIENT_VERSION=…" before running test.sh to override
-# green "Henceforth: CLIENT_VERSION=$CLIENT_VERSION\n"
+green "Beforehand: CLIENT_IMAGE=$CLIENT_IMAGE"
+# Default learn-ocaml-client image
+: ${CLIENT_IMAGE:=ocamlsf/learn-ocaml-client}
+# Do "export CLIENT_IMAGE=…" before running test.sh to override
+green "Henceforth: CLIENT_IMAGE=$CLIENT_IMAGE\n"
+
+green "Beforehand: CLIENT_UID=$CLIENT_UID"
+# Default learn-ocaml-client UID
+: ${CLIENT_UID:=1000}
+# Do "export CLIENT_UID=…" before running test.sh to override
+green "Henceforth: CLIENT_UID=$CLIENT_UID\n"
+
+green "Beforehand: CLIENT_PATH=$CLIENT_PATH"
+# Default learn-ocaml-client final path
+: ${CLIENT_PATH:=/home/coq/.local/bin/learn-ocaml-client}
+# Do "export CLIENT_PATH=…" before running test.sh to override
+green "Henceforth: CLIENT_PATH=$CLIENT_PATH\n"
 
 green "Beforehand: LEARNOCAML_VERSION=$LEARNOCAML_VERSION"
 # Default learn-ocaml version
-: ${LEARNOCAML_VERSION:=0.11}
+: ${LEARNOCAML_VERSION:=dev}
 # Do "export LEARNOCAML_VERSION=…" before running test.sh to override
 green "Henceforth: LEARNOCAML_VERSION=$LEARNOCAML_VERSION\n"
 
+green "Beforehand: EMACS_IMAGE=$EMACS_IMAGE"
+# Default emacs image
+: ${EMACS_IMAGE:=opam-emacs}
+# Do "export EMACS_IMAGE=…" before running test.sh to override
+green "Henceforth: EMACS_IMAGE=$EMACS_IMAGE\n"
+
 SERVER_NAME="learn-ocaml-server"
+EMACS_NAME="emacs-client"
+
+extract_client () {
+    mkdir -p dist
+    chmod 777 dist
+    # Extract the learn-ocaml-client binary
+    docker run --rm --entrypoint '' -v "$PWD/dist:/dist" \
+      "$CLIENT_IMAGE:$LEARNOCAML_VERSION" /bin/sh -c \
+      "set -ex && exec cp \$(which learn-ocaml-client) /dist/"
+    sudo chown -Rv "$CLIENT_UID:$CLIENT_UID" dist
+    sudo chmod 755 dist
+}
 
 # run a server in a docker container
 run_server () {
@@ -63,20 +93,39 @@ stop_server () {
       docker stop "$SERVER_NAME" )
 }
 
+# run an emacs in a docker container
+run_emacs () {
+    # Run the server in background
+    docker run -d -i --init --rm --name="$EMACS_NAME" -v "$PWD/dist:/dist" \
+      "$EMACS_IMAGE" /bin/bash
+    ret=$?
+    if [ "$ret" -ne 0 ]; then
+        red "PROBLEM, 'docker run -d ...' failed with exit status $ret"
+        exit $ret
+    fi
+}
+
+stop_emacs () {
+    green "Stopping emacs..."
+    ( set -x && \
+      docker stop "$EMACS_NAME" )
+}
+
 assert () {
     if [ $# -ne 1 ]; then
         red "ERROR, assert expects a single arg (the code to run)"
         exit 1
     fi
-    docker exec -i "$SERVER_NAME" /bin/sh -c "
+    docker exec -i "$EMACS_NAME" /bin/bash -c "
 set -ex
 $1
 "
     ret=$?
     if [ "$ret" -ne 0 ]; then
         red "FAILURE, this shell command returned exit status $ret:
-\$ docker exec -i $SERVER_NAME /bin/sh -c '$1'\n"
-        stop_server  # optional...
+\$ docker exec -i $EMACS_NAME /bin/bash -c '$1'\n"
+        stop_emacs
+        stop_server
         exit $ret
     fi
 }
@@ -84,8 +133,14 @@ $1
 ###############################################################################
 
 run_server
+extract_client
+run_emacs
 
-# assert "emacs --batch --eval '(pp (+ 2 2))'"
+assert "emacs --batch --eval '(pp (+ 2 2))'"
+
+assert "cp -f /dist/learn-ocaml-client \"$CLIENT_PATH\""
+
+assert "learn-ocaml-client --help"
 
 assert "
 cd /build/tests
@@ -93,4 +148,5 @@ learn-ocaml-client init --server=http://localhost:8080 test test
 emacs --batch -l ert -l init-tests.el -l /build/learn-ocaml.el -l learn-ocaml-tests.el -f ert-run-tests-batch-and-exit
 "
 
+stop_emacs
 stop_server
